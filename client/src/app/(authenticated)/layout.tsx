f0b60e1a6f3dc5872dc40440f8321ff51a763206
eyalyehiely <eyalwork0@gmail.com>
2025-05-13 19:02:31 +0300

    fixing layout component

---
+++ client/src/app/(authenticated)/layout.tsx
@@ -0,0 +1,28 @@
+import { auth } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
+import { Header } from "@/components/Header"
+import { Sidebar } from "@/components/Sidebar"
+
+export default function AuthenticatedLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect('/');
+  }
+
+  return (
+    <div className="min-h-screen">
+      <Header />
+      <div className="flex">
+        <Sidebar />
+        <main className="flex-1 p-6">
+          {children}
+        </main>
+      </div>
+    </div>
+  )
+} 
